1aa9a99d45beffcd9d66b835dd2d650b0eba7e5c
MOLINA <Molina@slon030s-MacBook-Air.local>
2020-01-24 22:58:47 -0500

    mapas

--- servidor/index.js
+++ servidor/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 
+const router = express.Router();
+
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
 const port = process.env.PORT || 3000;
 
 app.use(cors());
@@ -12,8 +13,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/estudiantes", (req, res) => {
   const event = require("./db.json").ubicaciones;
-  res.send(event);
+  // res.send(event);
+  res.json(event)
 });
+
+router.route('/estudiantes')
+.get((req, res)=>{
+  Mapas.find((err, mapas)=>{
+    if(err){
+      return res.send(err);
+    }
+    return res.json(mapas)
+  });
+})
+
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
